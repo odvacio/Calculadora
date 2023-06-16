14c4cef97c88a949723db791488c76cbfe57186f
Oscar Daniel Vacio Loera <ovacio@itszo.edu.mx>
2023-06-15 18:27:21 -0600

    Calculadora

---
+++ calcu.py
@@ -0,0 +1,19 @@
+n1 = float(input("Introduce tu primer número: ") )
+n2 = float(input("Introduce tu segundo número: ") )
+
+opcion = 0
+while True:
+    print("""
+    Dime, ¿qué quieres hacer?
+    
+    1) Sumar los dos números
+    2) Restar los dos números
+    3) Multiplicar los dos números
+    4) Cambiar los números elegidos
+    5) Apagar calculadora
+    """)
+    opcion = int(input("Elige una opción: ") )     
+
+
+    else:
+        print("Opción incorrecta")
